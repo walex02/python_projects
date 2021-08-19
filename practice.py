79ddb59f78d5ad8829d3f70778fddd43a25e9359
walex02 <walex.akinkunmi@gmail.com>
2021-08-19 03:20:41 +0100

    update

--- practice.py
+++ practice.py
@@ -1,3 +1,12 @@
 #list=[],tuple=(),dictionary={}
-name="Yusuf"
-print(name)
+#name="Yusuf"
+#print(name)
+var1=20
+var2=1.34
+var3="Hello World"
+#var4=3 + 4j
+#print(var4,type(var4))
+print(var1,var2,var3)
+print(type(var1),type(var2),type(var3))
+a,b,c=["akin","ahmed,"yusuf"]
+print[(b,a,c)]
